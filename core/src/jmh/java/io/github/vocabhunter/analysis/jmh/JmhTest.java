d27f74318f3187d1b84e5cdf5e4b3390deb265c5
Adam Carroll <AdamCarroll@users.noreply.github.com>
2018-03-13 22:30:19 +0100

    Improved JMH test use of annotations

--- core/src/jmh/java/io/github/vocabhunter/analysis/jmh/JmhTest.java
+++ core/src/jmh/java/io/github/vocabhunter/analysis/jmh/JmhTest.java
@@ -4,20 +4,26 @@
 
 package io.github.vocabhunter.analysis.jmh;
 
-import io.github.vocabhunter.analysis.session.SessionWordsTool;
-import io.github.vocabhunter.analysis.session.SessionWordsToolImpl;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.github.vocabhunter.analysis.file.FileStreamer;
+import io.github.vocabhunter.analysis.session.EnrichedSessionState;
+import io.github.vocabhunter.analysis.simple.SimpleAnalyser;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
+@State(Scope.Benchmark)
+@SuppressFBWarnings("DMI_HARDCODED_ABSOLUTE_FILENAME")
 public class JmhTest {
-    @Benchmark
-    public List<String> runTest() {
-        Path file = Paths.get("/full/path/sample.wordy");
-        SessionWordsTool target = new SessionWordsToolImpl();
+    private final FileStreamer streamer = new FileStreamer(new SimpleAnalyser());
 
-        return target.knownWords(file);
+    @Benchmark
+    public EnrichedSessionState runTest() {
+        Path file = Paths.get("big.txt");
+
+        return streamer.createNewSession(file);
     }
 }
